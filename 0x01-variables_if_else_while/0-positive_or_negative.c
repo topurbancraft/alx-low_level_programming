46a56c13e5d72ac95d2b35eab7e5547daf354a4d
topurbancraft <auchennamagnus@gmail.com>
2022-03-12 14:16:00 -0800

    hello world

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,17 +12,17 @@ int main(void)
 int n;
 $rand(time(0));
 n = rand() - RAND_MAX / 2;
-IF (N == 0)
+if (n == 0)
 {
-printf("%i is zero\n", n);
+printf("%d is zero\n", n);
 }
 else if (n < 0)
 {
-printf("%i is negative/n", n);
+printf("%d is negative/n", n);
 }
 else
 {
-printf("%i is positive\n", n);
+printf("%d is positive\n", n);
 }
 return (0);
 }
