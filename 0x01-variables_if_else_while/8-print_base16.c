1cf264bc9869abbee7cbdb2468ddcb2f36a0f4ba
topurbancraft <auchennamagnus@gmail.com>
2022-03-14 08:12:34 -0700

    new commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int n;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+for (n = 97; n < 103; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
