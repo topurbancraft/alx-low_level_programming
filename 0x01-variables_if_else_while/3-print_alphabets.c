b9cb10066ea2d5234c4c5c815de08852e9d94295
topurbancraft <auchennamagnus@gmail.com>
2022-03-14 06:50:47 -0700

    new commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - prints lower and uppercase alphabets 
+ *followed by a new line 
+ *Return:Always 0 
+ */
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z' i++)
+{
+putchar(i);
+}
+for (i = 'A'; i <= 'Z'; i++)
+}
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
