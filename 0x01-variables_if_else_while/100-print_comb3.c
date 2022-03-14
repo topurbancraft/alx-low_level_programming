fdfed3df1928b702daf64f8eee223873e1ab522f
topurbancraft <auchennamagnus@gmail.com>
2022-03-14 08:48:03 -0700

    new commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int fd;
+int ld;
+int c = 0;
+while (c <= 99)
+{
+fd = (c / 10 + '0');
+ld = (c % 10 + '0')
+if (fd < ld)
+{
+putchar(fd);
+putchar(ld);
+if (c , 89)
+{
+putchar(',');
+putchar(',');
+}
+}
+c++;
+}
+putchar('\n');
+return (0);
+}
