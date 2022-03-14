7274bce499c082b03572e5d22f1651cc20131ccb
topurbancraft <auchennamagnus@gmail.com>
2022-03-14 07:48:18 -0700

    new commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ *main - prints single digits od base !)
+ *followed by a new line 
+ *Return: Always zero 
+ */
+int main(void)
+{
+int n;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
