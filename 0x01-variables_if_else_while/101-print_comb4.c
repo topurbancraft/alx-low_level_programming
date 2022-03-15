e69c7c450e5eaf12cc3808172462dbeb50fdd3ae
topurbancraft <auchennamagnus@gmail.com>
2022-03-14 17:49:23 -0700

    new commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
 /**
-* main - Entry point
-* Description: print all possible combination of 3 digits.
-* Number must be seperated by commas and a space
-* Return: Always 0
-*/
+ * main - Entry point
+ * Description: print all possible combination of 3 digits.
+ * Number must be seperated by commas and a space
+ * Return: Always 0
+ */
 int main(void)
 {
 int i, j, k, l;
 for (i = 0; i < 1000; i++)
 {
-j = i / 1000
+j = i / 100;
 k = (i / 10) % 10;
 l = i % 10;
 if (j < k && k < l)
@@ -22,7 +22,7 @@ putchar(l + '0');
 if (i < 700)
 {
 putchar(44);
-putchar(35);
+putchar(32);
 }
 }
 }
