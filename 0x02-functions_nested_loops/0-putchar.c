3cbd13c225041cc850f75fde3fc8ae1aab425555
topurbancraft <auchennamagnus@gmail.com>
2022-03-16 15:02:10 -0700

    new commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main- prints _putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char text[] = "_putchar";
+int c;
+for (c = 0; c < 8; c++)
+{
+_putchar(text[c]);
+}
+_putchar('\n');
+return (0);
+}
