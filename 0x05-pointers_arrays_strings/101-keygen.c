552924408aadbbb11ea89815b552837446250ea0
topurbancraft <auchennamagnus@gmail.com>
2022-03-22 03:02:06 -0700

    new task

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,16 +8,15 @@
   */
 int main(void)
 {
-	int sum;
-	char c;
-
-	srand(time(NULL));
-	while (sum <= 2645)
-	{
-		c = rand() % 128;
-		sum += c;
-		putchar(c);
-	}
-	putchar(2772 - sum);
-	return (0);
+int sum;
+char c;
+srand(time(NULL));
+while (sum <= 2645)
+{
+c = rand() % 128;
+sum += c;
+putchar(c);
+}
+putchar(2772 - sum);
+return (0);
 }
