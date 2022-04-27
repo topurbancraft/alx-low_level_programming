15d3b8ba059d67c6b3fbed14a3751bdc9374d094
topurbancraft <auchennamagnus@gmail.com>
2022-04-27 11:11:45 -0700

    new alx task

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,21 @@
-#include "lists.h"
 #include <stdio.h>
-
+#include <stdlib.h>
+#include "lists.h"
 /**
- * print_listint - Prints all the elements of a listint_t list.
- * @h: A pointer to the head of the ltint_t *h)
-{
-	size_t nodes = 0;
-	while (h)
-	{
-		nist_t list.
- *
- * Return: The number of nodes in the list_t list.
+ *print_listint - print all elements of a listint_t list
+ *@h: header
+ *Return: number of nodes
  */
-size_t print_listint(const lisodes++;
-		printf("%d\n", h->n);
-		h = h->next;
-	}
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *temp;
 
-	return (nodes);
+	for (temp = h; temp != NULL; temp = temp->next)
+	{
+		printf("%d\n", temp->n);
+		count++;
+	}
+	return (count);
+
 }
