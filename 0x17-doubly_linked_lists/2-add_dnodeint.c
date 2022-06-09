9c62531345e4933ea54ac4cd1c6c32b9fd28dee8
topurbancraft <auchennamagnus@gmail.com>
2022-06-08 23:18:12 -0700

    New commit

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * *add_dnodeint - adds a node to the beginning of a double linked list
+ * @head: linked list head
+ * @n: int element of the linked list
+ * Return: number of elements
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *new_node;
+
+new_node = malloc(sizeof(dlistint_t));
+
+if (new_node == NULL)
+{
+free(new_node);
+return (NULL);
+}
+new_node->n = n;
+new_node->prev = NULL;
+new_node->next = *head;
+
+if (*head)
+(*head)->prev = new_node;
+*head = new_node;
+
+return (new_node);
+}
